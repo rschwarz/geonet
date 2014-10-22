ef758535058645d88a4c5a6c0382810514bc75cb
Robert Schwarz <schwarz@zib.de>
2014-10-22 11:05:23 +0200

    add more tests for network

--- tests/test_network.py
+++ tests/test_network.py
@@ -1,4 +1,4 @@
-from geonet.network import Net
+from geonet.network import Net, SteinerTree
 
 def test_Net():
     nodes = ['a', 'b', 'c']
@@ -7,3 +7,14 @@ def test_Net():
     net = Net(nodes, arcs)
     assert set(net.get_nodes()) == set(nodes)
     assert set(net.get_arcs()) == set(arcs)
+
+def test_SteinerTree():
+    nodes = ['a', 'b', 'c', 's']
+    arcs = [('a', 's'), ('b', 's'), ('c', 's')]
+    pos = {'a': (0,0), 'b': (1,0), 'c': (0,1)}
+
+    tree = SteinerTree(nodes, arcs, pos)
+
+    for n in 'abc':
+        assert tree.is_terminal(n)
+    assert tree.is_steiner('s')
