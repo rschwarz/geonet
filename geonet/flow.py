e1316c13792c7606508eeb186b337fbb4e5b2ca0
Robert Schwarz <schwarz@zib.de>
2015-06-11 13:44:42 +0200

    add make_forward_flow

--- geonet/flow.py
+++ geonet/flow.py
@@ -6,6 +6,8 @@ import networkx as nx
 import numpy as np
 import scipy as sp
 
+from geonet.network import SteinerTree
+
 def find_arc_flow(net, demand):
     '''Determine flow on arcs.
 
@@ -29,3 +31,30 @@ def find_arc_flow(net, demand):
 
     sol = sp.sparse.linalg.spsolve(im, dem)
     return dict(zip(edgs, sol))
+
+def make_forward_flow(tree, flow):
+    '''Create tree with arcs oriented for positive, forward flow.
+
+    args:
+    - tree: a SteinerTree
+    - flow: a dict from arc to (signed) flow value
+
+    returns:
+    - a new SteinerTree
+    - a new flow dict with only positive flow values
+    '''
+    new_nodes = tree.get_nodes()
+    new_arcs = []
+    new_pos = tree.get_terminal_positions()
+    new_flow = {}
+
+    for u, v in tree.get_arcs():
+        if flow[u, v] >= 0.0:
+            new_arcs.append((u, v))
+            new_flow[u, v] = flow[u, v]
+        else:
+            new_arcs.append((v, u))
+            new_flow[v, u] = -1.0 * flow[u, v]
+
+    new_tree = SteinerTree(new_nodes, new_arcs, new_pos)
+    return new_tree, new_pos
--- tests/test_flow.py
+++ tests/test_flow.py
@@ -1,7 +1,7 @@
 import pytest
 
-from geonet.network import Net
-from geonet.flow import find_arc_flow
+from geonet.network import Net, SteinerTree
+from geonet.flow import find_arc_flow, make_forward_flow
 
 def test_treeflow():
     '''Check that (unique) flow values are computed correctly.'''
@@ -26,3 +26,23 @@ def test_imbalance():
     with pytest.raises(AssertionError) as e:
         flows = find_arc_flow(net, demand)
     assert 'flow not balanced' in str(e.value)
+
+def test_make_forward_flow():
+    O = (0, 0)
+    tree = SteinerTree('abcs', ['as', 'bs', 'cs'], {'a':O, 'b':O, 'c':O})
+    flow = {('a', 's'):20, ('b','s'):-5, ('c','s'):-15}
+
+    new_tree, new_flow = make_forward_flow(tree, flow)
+    assert set(tree.get_nodes()) == set(new_tree.get_nodes())
+    assert set(tree.get_terminal_nodes()) == set(new_tree.get_terminal_nodes())
+    assert len(tree.get_arcs()) == len(new_tree.get_arcs())
+    assert tree.get_terminal_positions() == new_tree.get_terminal_positions()
+
+    assert ('a', 's') in new_tree.get_arcs()
+    assert ('s', 'a') not in new_tree.get_arcs()
+
+    assert ('s', 'b') in new_tree.get_arcs()
+    assert ('b', 's') not in new_tree.get_arcs()
+
+    assert ('s', 'c') in new_tree.get_arcs()
+    assert ('c', 's') not in new_tree.get_arcs()
